a3ea1b899a2f8d39dfc8a347ab6ae862dfa93f04
ericgchan <eric@apen4ej.org>
2019-05-29 21:42:43 -0700

    submit unit 2 assignment 1

--- Assignments/Unit_2/Assignment_1/index.js
+++ Assignments/Unit_2/Assignment_1/index.js
@@ -2,26 +2,60 @@
 
 // 1. Perform three basic math operations
 
-
+console.log(5 + 3);
+console.log(3 * 4);
+console.log(9 / 2);
 
 // 2. Create a variable called "myNum" and assign it a value of 3
 
+let myNum = 3;
+
 // 3. Output/print/return the value of the "myNum" variable
+
+console.log(myNum);
 
 // 4. Assign "mynum" a value of 7
 
+myNum = 7;
+
 // 5. Output/print/return the value of "myNum"
+
+console.log(myNum);
 
 // 6. Increase the value of "myNum" by 3
 
+myNum = myNum + 3;
+
 // 7. Output/print/return the value of "myNum"
+
+console.log(myNum);
 
 // 8. Create another variable called "myOtherNum" with value 5
 
+let myOtherNum = 5;
+
 // 9. Compare "myNum" and "myOtherNum". Are they equal? Your code should return 'true' or 'false'
+
+if (myNum === myOtherNum) {
+    console.log('true');
+}
+else {
+    console.log('false');
+}
 
 // 10. Increase the value of "myOtherNum" by 5
 
+myOtherNum = myOtherNum + 5;
+
 // 11. Compare "myNum" and "myOtherNum". Are they equal? Your code should return 'true' or 'false'
 
+if (myNum === myOtherNum) {
+    console.log('true');
+}
+else {
+    console.log('false');
+}
+
 // 12. Create a third variable called "myString" with value "10" (note: "2" should be a string, NOT a number)
+
+let myString = '10';
